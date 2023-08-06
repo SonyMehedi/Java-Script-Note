bdafb2805c5d4608f5c78a77ed9f29262211341f
sonymehedi <mehediahmedsony@gmail.com>
2023-08-07 00:47:51 +0600

    first commit

--- variable.js
+++ variable.js
@@ -1,6 +1,17 @@
-vari+able= veriable
+//vary+able= variable
+
+//numeric
 var price = 11;
 var age = 22;
 var temperature = 38;
 var many = 100;
 var wight =500;
+//string
+var name = "korim Mima";
+avr location = "bandor ban";
+var spacial = "tura fati"; 
+
+//Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
