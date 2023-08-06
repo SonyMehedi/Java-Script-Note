301a38b4ba79040c997fef9fcdc49e327509c47c
sonymehedi <mehediahmedsony@gmail.com>
2023-08-07 01:08:22 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,12 @@
+var price = 29;
+// 1. variable name can not be any keyword
+var true = 22;
+var return = true;
+
+// 2. variable name has to be in noe word.No space
+avr my home address = "New california";
+// 3. variable name can not have quotation
+avr "name" = "tom tom";
+// 4. variable name can not start with a number but can and with a number
+var 99club = 134358;
+var club34= 3545; 
